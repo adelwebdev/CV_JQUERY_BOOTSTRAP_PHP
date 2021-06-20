3ba1659f1aa59122c58d9a18653c55a51229d127
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-06-21 01:30:47 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // typed
+var typed3 = new Typed("#typed3", {
+  strings: [
+    "My strings are: <i>strings</i> with",
+    "My strings are: <strong>HTML</strong>",
+    "My strings are: Chars &times; &copy;",
+  ],
+  typeSpeed: 0,
+  backSpeed: 0,
+  smartBackspace: true, // this is a default
+  loop: true,
+});
