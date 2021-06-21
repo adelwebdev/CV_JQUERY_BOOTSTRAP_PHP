707bd6a3d3337cb9a3352436e412bae1c5030f41
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-06-21 18:12:50 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+// NAVBAR
+let lastScrollTop = 0;
+navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", function () {
+  const scrollTop =
+    window.pageTOffset || this.document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = "-50px";
+  } else {
+    navbar.style.top = "0";
+  }
+  lastScrollTop = scrollTop;
+});
+
 // typed
 var typed = new Typed(".typed", {
-  strings: ["snna", "cllc", "d", "mec"],
+  strings: [" Programmeur", " Dévéloppeur", " Docteur"],
   typeSpeed: 34,
   backSpeed: 3,
   smartBackspace: true, // this is a default
