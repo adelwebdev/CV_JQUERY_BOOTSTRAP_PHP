e47d5b4000d18674642613f284e568818ca2a66e
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-06-21 23:57:51 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -14,11 +14,42 @@ window.addEventListener("scroll", function () {
   lastScrollTop = scrollTop;
 });
 
-// Typed---------------------------------------------------------
+// TYPED--------------------------------------------------------
 var typed = new Typed(".typed", {
   strings: [" Programmeur", " Dévéloppeur", " Docteur"],
   typeSpeed: 34,
   backSpeed: 3,
   smartBackspace: true, // this is a default
   loop: true,
+});
+
+// COMPTERU LIVE
+let compteur = 0;
+$(window).scroll(function () {
+  const top = $(".counter").offset().top - window.innerHeight;
+
+  if (counter == 0 && $(window).scrollTop() > top) {
+    $(".counter-value").each(function () {
+      let $this = $(this),
+        countTo = $this.attr("data-count");
+      $({
+        countNum: $this.text(),
+      }).animate(
+        {
+          countNum: countTo,
+        },
+        {
+          duration: 10000,
+          easing: "swing",
+          step: function () {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function () {
+            $this.text(this.countNum);
+          },
+        }
+      );
+    });
+    compteur = 1;
+  }
 });
